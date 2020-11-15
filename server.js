8c070535c07d71c0b867e9438da2fc07386ccba1
Aleksei <alekseig1803@gmail.com>
2020-11-16 00:15:46 +0300

    Changed port

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const server = http.createServer((req, res) => {
     res.end(body)
 })
 
-server.listen(3000)
+const port = process.env.PORT || 3000
+server.listen(port)
 
-console.log('Server started!')
+console.log(`Server started on port ${port}!`)
 
